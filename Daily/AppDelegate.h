e8dcc3fb59a28ccc8d6068dbb63206faa4ca1ff9
lixingdong <lixingdong@foundersc.com>
2018-12-21 21:54:14 +0800

    init project

---
+++ Daily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Daily
+//
+//  Created by 李兴东 on 2018/12/21.
+//  Copyright © 2018年 xingshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Daily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Daily
+//
+//  Created by 李兴东 on 2018/12/21.
+//  Copyright © 2018年 xingshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
